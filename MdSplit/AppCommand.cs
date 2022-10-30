a9adb9047ecb9eab685e2b666305b60c989eae6b
Damir Arh <damir.arh@gmail.com>
2022-10-30 09:26:19 +0100

    Implement functionality

---
+++ MdSplit/AppCommand.cs
@@ -0,0 +1,55 @@
+﻿using McMaster.Extensions.CommandLineUtils;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace MdSplit;
+
+[Command(Description = "Split a markdown file into multiple files.")]
+public class AppCommand
+{
+    [Required]
+    [Option(Description = "Required. The markdown file to split.")]
+    public string Input { get; set; } = null!;
+
+    [Required]
+    [Option(Description = "Required. The output directory.")]
+    public string Output { get; set; } = null!;
+
+    public int OnExecute(IConsole console)
+    {
+        try
+        {
+            var lines = File.ReadAllLines(this.Input);
+
+            string? filename = null;
+            StringBuilder contents = new();
+            foreach (var line in lines)
+            {
+                if (line.StartsWith("# "))
+                {
+                    if (filename != null)
+                    {
+                        File.WriteAllText(Path.Combine(this.Output, filename), contents.ToString());
+                        contents.Clear();
+                    }
+                    filename = string.Concat(line.AsSpan(2), ".md");
+                }
+                else
+                {
+                    contents.AppendLine(line);
+                }
+            }
+            if (filename != null)
+            {
+                File.WriteAllText(Path.Combine(this.Output, filename), contents.ToString());
+            }
+
+            return 0;
+        }
+        catch (Exception ex)
+        {
+            console.WriteLine(ex);
+            return 1;
+        }
+    }
+}
--- MdSplit/Program.cs
+++ MdSplit/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using MdSplit;
+using Microsoft.Extensions.Hosting;
+
+Host.CreateDefaultBuilder()
+    .RunCommandLineApplicationAsync<AppCommand>(args);
